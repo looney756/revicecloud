6c07f454ae8cfc80a29b30eb3dc4df0c8dd08cb4
looney756 <r4mzya@gmail.com>
2021-01-17 15:47:26 +0600

    React added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require(`express`);
 const bcrypt = require(`bcryptjs`);
 const { check, validationResult } = require(`express-validator`);
+const config = require(`config`);
+const jwt = require(`jsonwebtoken`);
 const router = Router();
 const User = require(`../models/User`);
 
@@ -14,9 +16,9 @@ router.post(
             .withMessage(`Minimum length is 4 symbols.`)
             .matches(`/^[A-Za-z][A-Za-z0-9]{4,12}$/gi`)
             .withMessage(`Only symbols and digits.`),
-        check(`password`, `Uncorrect password`)
+        check(`password`, `Uncorrect password.`)
             .isLength({ min: 6, max: 24 })
-            .withMessage(`Minimum length is 4 & Maximum length is 24`),
+            .withMessage(`Minimum length is 4 & Maximum length is 24.`),
     ],
     async (req, res) => {
         try {
@@ -24,7 +26,7 @@ router.post(
             if (!errors.isEmpty()) {
                 return res.status(400).json({
                     errors: errors.array(),
-                    message: `Uncorrect data`,
+                    message: `Uncorrect data.`,
                 });
             }
             const { login, password } = req.body;
@@ -47,6 +49,48 @@ router.post(
     }
 );
 
-router.post("/login", async (req, res) => {});
+router.post(
+    "/login",
+    [
+        check(`login`, `Uncorrect login`).exists(),
+        check(`password`, `Type a password`).exists(),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: `Uncorrect login`,
+                });
+            }
+            const { login, password } = req.body;
+
+            const user = await User.findOne({ login });
+
+            if (!user) {
+                return res.status(400).json({ message: `User not found.` });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ message: `Uncorrect password, try again.` });
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get(`jwtSecret`),
+                { expiresIn: `3h` }
+            );
+
+            res.json({ token, userId: user.id });
+        } catch (e) {
+            res.status(500).json({ message: `/auth/register\n${e.message}` });
+        }
+    }
+);
 
 module.exports = router;
